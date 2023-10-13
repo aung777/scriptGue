d32f5ceaeed6050da157d351b1c8a8df2cb2385d
aung777 <46964841+aung777@users.noreply.github.com>
2023-10-13 18:19:48 +0700

    sad

--- 4play show komen.js
+++ 4play show komen.js
@@ -4,6 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
+// @downloadURL https://raw.githubusercontent.com/aung777/scriptGue/main/4play%20show%20komen.js
+// @updateURL   https://raw.githubusercontent.com/aung777/scriptGue/main/4play%20show%20komen.js
 // @match        https://4play.to/koleksi/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=4play.to
 // @grant        none

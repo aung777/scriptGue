7a3f27967d45a1195043dc1bdb687cfb555d3d4d
aung777 <46964841+aung777@users.noreply.github.com>
2023-10-14 11:12:02 +0700

    sad

--- nhen show EN only.js
+++ nhen show EN only.js
@@ -2,50 +2,53 @@
 // @name         nhen show EN only
 // @version      1.0.1
 // @description  try to take over the world!
-// @author       You
+// @author       AING
 // @match        https://nhentai.net/*
 // @icon         https://cdn-icons-png.flaticon.com/128/75/75277.png
+// @downloadURL https://raw.githubusercontent.com/aung777/scriptGue/main/nhen%20show%20EN%20only.js
+// @updateURL   https://raw.githubusercontent.com/aung777/scriptGue/main/nhen%20show%20EN%20only.js
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  "use strict";
-
-  // Membuat elemen tombol
-  const toggleButton = document.createElement("button");
-  toggleButton.textContent = "show ALL";
-
-  // Inisialisasi status tersembunyi
-  let isHidden = false;
-  const myInterval = setInterval(makeHidden, 1000);
-
-  function makeHidden() {
-    const elementsWithout12227 = document.querySelectorAll(
-      '.gallery:not([data-tags*="12227"])'
-    );
-
-    elementsWithout12227.forEach((element) => {
-      if (isHidden) {
-        element.classList.add("hidden");
-      } else {
-        element.classList.remove("hidden");
-      }
+    "use strict";
+  
+    // Membuat elemen tombol
+    const toggleButton = document.createElement("button");
+    toggleButton.textContent = "show ALL";
+  
+    // Inisialisasi status tersembunyi
+    let isHidden = false;
+    const myInterval = setInterval(makeHidden, 100);
+  
+    function makeHidden() {
+      const elementsWithout12227 = document.querySelectorAll(
+        '.gallery:not([data-tags*="12227"])'
+      );
+  
+      elementsWithout12227.forEach((element) => {
+        if (isHidden) {
+          element.classList.add("hidden");
+        } else {
+          element.classList.remove("hidden");
+        }
+      });
+    }
+  
+    // Menambahkan event listener ke tombol
+    toggleButton.addEventListener("click", function () {
+      // Cari semua elemen dengan kelas "gallery" yang tidak memiliki atribut "data-tags" yang berisi "12227"
+  
+      makeHidden();
+  
+      // Memperbarui status tersembunyi
+      isHidden = !isHidden;
+  
+      toggleButton.innerText = isHidden ? "EN only" : "show ALL";
     });
-  }
-
-  // Menambahkan event listener ke tombol
-  toggleButton.addEventListener("click", function () {
-    // Cari semua elemen dengan kelas "gallery" yang tidak memiliki atribut "data-tags" yang berisi "12227"
-
-    makeHidden();
-
-    // Memperbarui status tersembunyi
-    isHidden = !isHidden;
-
-    toggleButton.innerText = isHidden ? "EN only" : "show ALL";
-  });
-
-  // Menambahkan tombol ke body halaman
-  tombollist = document.querySelector(".menu.left");
-  tombollist.appendChild(toggleButton);
-})();
+  
+    // Menambahkan tombol ke body halaman
+    var tombollist = document.querySelector(".menu.left");
+    tombollist.appendChild(toggleButton);
+  })();
+  

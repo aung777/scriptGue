101cf4668795317428a6c5951190da75ddb6782c
aung777 <46964841+aung777@users.noreply.github.com>
2023-10-16 10:18:09 +0700

    asas

--- Youtube Shorts Autoscroll [chatgpt].js
+++ Youtube Shorts Autoscroll [chatgpt].js
@@ -12,6 +12,86 @@
 // ==/UserScript==
 
 (function () {
+  // Gaya CSS untuk tombol toggle switch
+  const style = `
+  button.yt-spec-button-shape-next.yt-spec-button-shape-next--filled.yt-spec-button-shape-next--overlay.yt-spec-button-shape-next--size-m {
+  height: 10px;
+}
+  ytd-reel-player-header-renderer {
+  padding: 0 !important;
+  margin: 0 !important;
+}
+
+ytd-reel-player-header-renderer * {
+  padding: 0 !important;
+  margin: 0 !important;
+  font-size: xx-small !important;
+  height: 20px;
+  opacity: 95%;
+}
+  .toggle-switch {
+    position: fixed;
+    z-index: 9999;
+    cursor: pointer;
+    font-size: 12px;
+    user-select: none;
+  }
+  .isHidden {
+  visibility: hidden;
+  }
+
+  .toggle-switch input {
+    display: none;
+  }
+
+  .toggle-slider {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 60px;
+    height: 30px;
+    background-color: #ccc;
+    border-radius: 15px;
+    transition: background-color 0.3s;
+    padding: 2px;
+  }
+
+  .toggle-slider p {
+    margin: 0;
+    margin-left: 4px;
+    margin-right: 1px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: bold;
+    color: white;
+  }
+
+  .slider-ball {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    width: 30px;
+    height: 30px;
+    background-color: white;
+    border-radius: 50%;
+    transition: transform 0.3s;
+  }
+
+  .toggle-switch input:checked + .toggle-slider {
+    background-color: green;
+  }
+
+  .toggle-switch input:checked + .toggle-slider .slider-ball {
+    transform: translateX(100%);
+  }
+`;
+
+  // Menambahkan gaya CSS ke head dokumen
+  const styleElement = document.createElement("style");
+  styleElement.innerHTML = style;
+  document.head.appendChild(styleElement);
+  const toggleQS = document.querySelector(".toggle-switch");
+
   let autoscrollEnabled = true;
 
   // Membuat tombol toggle switch
@@ -45,101 +125,11 @@
 
   // Fungsi untuk mengubah status autoscroll saat toggle diubah
   toggleInput.addEventListener("input", function () {
-    swithFunction();
+    autoscrollEnabled = !autoscrollEnabled;
+    changeColor();
+    // console.log("keklik");
   });
 
-  // Gaya CSS untuk tombol toggle switch
-  const style = `
-    button.yt-spec-button-shape-next.yt-spec-button-shape-next--filled.yt-spec-button-shape-next--overlay.yt-spec-button-shape-next--size-m {
-    height: 10px;
-}
-    ytd-reel-player-header-renderer {
-    padding: 0 !important;
-    margin: 0 !important;
-}
-
-ytd-reel-player-header-renderer * {
-    padding: 0 !important;
-    margin: 0 !important;
-    font-size: xx-small !important;
-    height: 20px;
-    opacity: 95%;
-}
-    .toggle-switch {
-      position: fixed;
-      z-index: 9999;
-      cursor: pointer;
-      font-size: 12px;
-      user-select: none;
-    }
-    .isHidden {
-    visibility: hidden;
-    }
-
-    .toggle-switch input {
-      display: none;
-    }
-
-    .toggle-slider {
-      position: relative;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      width: 60px;
-      height: 30px;
-      background-color: #ccc;
-      border-radius: 15px;
-      transition: background-color 0.3s;
-      padding: 2px;
-    }
-
-    .toggle-slider p {
-      margin: 0;
-      margin-left: 4px;
-      margin-right: 1px;
-      font-family: Arial, Helvetica, sans-serif;
-      font-weight: bold;
-      color: white;
-    }
-
-    .slider-ball {
-      position: absolute;
-      top: 2px;
-      left: 2px;
-      width: 30px;
-      height: 30px;
-      background-color: white;
-      border-radius: 50%;
-      transition: transform 0.3s;
-    }
-
-    .toggle-switch input:checked + .toggle-slider {
-      background-color: green;
-    }
-
-    .toggle-switch input:checked + .toggle-slider .slider-ball {
-      transform: translateX(100%);
-    }
-  `;
-
-  // Menambahkan gaya CSS ke head dokumen
-  const styleElement = document.createElement("style");
-  styleElement.innerHTML = style;
-  document.head.appendChild(styleElement);
-  const toggleQS = document.querySelector(".toggle-switch");
-
-  //  (async () => {
-  //         let count_before = await GM.getValue('autoscrollstatus', false);
-
-  //         // Note awaiting the set -- required so the next get sees this set.
-  //         await GM.setValue('autoscrollstatus', false);
-
-  //         // Get the value again, just to demonstrate order-of-operations.
-  //         let count_after = await GM.getValue('autoscrollstatus');
-
-  //         console.log('autoscrollEnabled', count_after, 'times');
-  //     })();
-  //     toggleInput.checked = GM.getValue('autoscrollEnabled');
 
   setInterval(function () {
     if (!autoscrollEnabled) {
@@ -172,22 +162,24 @@ ytd-reel-player-header-renderer * {
   //cek pathname
   function handleVideoChanges() {
     if (window.location.pathname.includes("shorts")) {
-        console.log("GM getvalue");
+      //   console.log("GM getvalue : " + GM.getValue("gmstatus", "false"));
+
       toggleQS.style.display = "block";
       // console.log('masuk ke if');
     } else {
       toggleQS.style.display = "none";
     }
   }
-//   function work switch toggle
-  function swithFunction() {
+  //   function work switch toggle
+  function changeColor() {
+    // console.log("autoscrollEnabled : " + autoscrollEnabled);
     if (autoscrollEnabled) {
-        toggleSlider.style.backgroundColor = "green";
-        sliderBall.style.transform = "translateX(100%)";
-      } else {
-        toggleSlider.style.backgroundColor = "red";
-        sliderBall.style.transform = "translateX(0)";
-      }  
+      toggleSlider.style.backgroundColor = "green";
+      sliderBall.style.transform = "translateX(100%)";
+    } else {
+      toggleSlider.style.backgroundColor = "red";
+      sliderBall.style.transform = "translateX(0)";
+    }
   }
 
   // Atur event listener untuk mendeteksi perubahan di subtree dokumen
@@ -203,6 +195,10 @@ ytd-reel-player-header-renderer * {
   // Mulai memantau subtree dokumen
   observer.observe(document, { subtree: true, childList: true });
 
+  window.onload = (event) => {
+    
+    changeColor();
+  };
   //setInterval(function() {
   //  console.log("curret time : " + document.querySelector(".html5-video-player").querySelector("video").currentTime
   //            + "|\n" + "durasi : " + document.querySelector(".html5-video-player").querySelector("video").duration);
